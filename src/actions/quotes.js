a8514216edac0e1833d9cd771fd7789a4aeb4d16
rumikoacopa <rumikorios@gmail.com>
2021-03-04 00:10:45 -0500

    Done.

--- src/actions/quotes.js
+++ src/actions/quotes.js
@@ -1,1 +1,39 @@
+
+import uuid from 'uuid';
+export const ADD_QUOTE = 'ADD_QUOTE'
+export const REMOVE_QUOTE = 'REMOVE_QUOTE'
+export const UPVOTE_QUOTE = 'UPVOTE_QUOTE'
+export const DOWNVOTE_QUOTE = 'DOWNVOTE_QUOTE'
 // TODO: Create action creators as defined in tests
+export const addQuote = (quote) => {
+  let id = uuid();
+  return {
+    type: ADD_QUOTE,
+    quote: {
+      id,
+      ...quote,
+      votes: 0
+    }
+  }
+}
+
+export const removeQuote = (quoteId) => {
+  return {
+    type: REMOVE_QUOTE,
+    quoteId
+  };
+};
+
+export const upvoteQuote = (quoteId) => {
+  return {
+    type: UPVOTE_QUOTE,
+    quoteId
+  };
+};
+
+export const downvoteQuote = (quoteId) => {
+  return {
+    type: DOWNVOTE_QUOTE,
+    quoteId
+  };
+};
